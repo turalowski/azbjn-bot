ac8d508765b417c88c019684087c758e4e1c2ef4
Tural Hajiyev <thajiyev9@outlook.com>
2021-08-18 04:53:05 +0400

    Is url

--- utils/checks.js
+++ utils/checks.js
@@ -1,14 +1,12 @@
 const url_validator = require('is-url');
-const regex = new RegExp(
-  '([a-zA-Zd]+://)?((w+:w+@)?([a-zA-Zd.-]+.[A-Za-z]{2,4})(:d+)?(/.*)?)',
-  'i'
-);
+
 const isMedia = content => content.attachments.size > 0;
 const includesText = content => content.content.length > 0;
 const isCorrectChannel = (content, channels) =>
   Object.values(channels).includes(content.channel.id);
 
-const isUrl = content => regex.test(content.content);
+const isUrl = content =>
+  content.content.includes('https://') || content.content.includes('http://');
 
 module.exports = {
   isMedia,
